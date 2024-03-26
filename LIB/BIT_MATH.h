b1a64c160220d3f3330f44bc5998a533896d1503
Ahmed Khaled <130765437+Ahmed-Khaled-Abdelmaksod@users.noreply.github.com>
2024-03-26 02:00:52 +0200

    Add files via upload

---
+++ LIB/BIT_MATH.h
@@ -0,0 +1,31 @@
+﻿/*
+* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< BIT MATH file >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ * @author : Ahmed Khaled Abdelmaksod Ebrahim
+ * @layer  : libraries
+ * @brief  : contains helpful macrofunctions to bitwise operations
+ */
+#ifndef _BIT_MATH_H_
+#define _BIT_MATH_H_
+/*
+	@brief : used to set a specified bit inside a port
+*/
+#define SET_BIT(PORT,PIN)      PORT |= (1<<PIN)
+
+
+/*
+	@brief : used to clear a specified bit inside a port
+*/
+#define CLEAR_BIT(PORT, PIN)    (PORT &= ~(1 << (PIN)))
+
+
+/*
+	@brief : used to toggle a specified bit inside a port
+*/
+#define TOGGLE_BIT(PORT,PIN)   (PORT = PORT ^ (1<<PIN))
+
+/*
+	@brief : used to get a specified bit inside a port
+*/
+#define GET_BIT(PORT,PIN)      (((PORT >> PIN) & 0x01))
+
+#endif
---
+++ LIB/STD_TYPES.h
@@ -0,0 +1,26 @@
+﻿/*
+ * <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< STD_TYPES file >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ * @author : Ahmed Khaled Abdelmaksod Ebrahim
+ * @layer  : libraries
+ * @brief  : contains standard types of data type to increase portability
+ */
+
+#ifndef _STD_TYPES_H_
+#define _STD_TYPES_H_
+
+
+typedef unsigned char       u8;
+typedef unsigned short      u16;
+typedef unsigned long int   u32;
+
+
+typedef signed char         s8;
+typedef signed short        s16;
+typedef signed long int     s32;
+
+
+typedef float  f32;
+typedef double f64;
+
+
+#endif
